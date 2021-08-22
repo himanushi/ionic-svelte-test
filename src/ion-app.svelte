16677748fb7c1fbb01f90f8e28b99a03d3ce0ac7
himanushi <himanushi@users.noreply.github.com>
2021-08-22 09:09:21 +0900

    ionic 試したけど build 出来なかった

--- src/ion-app.svelte
+++ src/ion-app.svelte
@@ -1,32 +1,22 @@
 <svelte:options tag="ion-app" />
 
 <script lang="ts">
+	import { defineCustomElements } from "@ionic/core/dist/esm-es5/loader.js";
+import "@ionic/core/css/ionic.bundle.css";
+import { onMount } from "svelte";
 	export let name: string;
+
+onMount(() => {
+
+	if (typeof window !== "undefined") {
+
+defineCustomElements(window);
+
+}
+})
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
